70dbf37a4b563cb7d247230a8559127b213bc3eb
kishan5 <barai.kishan@gmail.com>
2025-07-08 01:28:32 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Analytics } from "@vercel/analytics/next";
+//import { Analytics } from "@vercel/analytics/next";
 import { useState, useEffect } from "react";
 
 export default function Home() {
